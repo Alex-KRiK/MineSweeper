88bc497fffd48a0ae6d2de927f374eeb85fc0ae1
Alexandr Gvozd <Alexandr.gvozd@gmail.com>
2017-05-19 03:59:33 +0300

    fixed the search for empty cells

--- js/script.js
+++ js/script.js
@@ -5,20 +5,21 @@ var eImages = [];
 var emptyCellsCount = 0;
 var minesCount = 13;
 var flagCount = 0;
-var PlaceMinesCount= 0; //count of placed mines
+var placeMinesCount= 0; //count of placed mines
 var CELL_MINE = 100;
 var arrayFieldNumbers = [];
+var row, col;
 
 //draw mine field
 var eContainer = document.getElementById("mine-field-container");
 var eTable = document.createElement("table");
 var eTbody = document.createElement("tbody");
 
-for (var row = 0; row < height; row++) {
+for (row = 0; row < height; row++) {
     var eTr = document.createElement("tr");
     eImages[row] = [];
 
-    for (var col = 0; col < width; col++) {
+    for (col = 0; col < width; col++) {
         var eTd = document.createElement("td");
         var eImg = document.createElement("img");
         eImg.src = "images/field.jpg";
@@ -41,73 +42,70 @@ eButton.addEventListener("click", function () {
 });
 
 //write in arrayFieldNumbers
-for (var row = 0; row < height; row++) {
+for (row = 0; row < height; row++) {
     arrayFieldNumbers[row] = [];
-    for (var col = 0; col < width; col++) {
-        arrayFieldNumbers[row][col] = 0;
+    for (col = 0; col < width; col++) {
+        arrayFieldNumbers[row][col] = { number: 0, mine: false };
     }
 }
-//document.write(arrayFieldNumbers.join());
 
 //place the mines on the playing field
-while (PlaceMinesCount != minesCount) {
-    var row = Math.floor(Math.random() * height);
-    var col = Math.floor(Math.random() * width);
+while (placeMinesCount != minesCount) {
+    row = Math.floor(Math.random() * height);
+    col = Math.floor(Math.random() * width);
 
-    if (arrayFieldNumbers[row][col] === CELL_MINE) continue;
-    arrayFieldNumbers[row][col] = CELL_MINE;
-    PlaceMinesCount++;
+    if (arrayFieldNumbers[row][col].mine) continue;
+    arrayFieldNumbers[row][col].mine = true;
+    placeMinesCount++;
 }
-//document.write(arrayFieldNumbers.join());
 
 //place the numbers around the mines
-for (var row = 0; row < height; row++) {
-    for (var col = 0; col < width; col++) {
+for (row = 0; row < height; row++) {
+    for (col = 0; col < width; col++) {
 
-        if (arrayFieldNumbers[row][col] === CELL_MINE) {
+        if (arrayFieldNumbers[row][col].mine) {
 
             //top diagonal left
-            if ((row > 0) && (col > 0) && (arrayFieldNumbers[row-1][col-1] != CELL_MINE)) {
-                arrayFieldNumbers[row-1][col-1]++;
+            if ((row > 0) && (col > 0) && (arrayFieldNumbers[row-1][col-1].mine === false)) {
+                arrayFieldNumbers[row-1][col-1].number++;
             }
             //top center
-            if ((row > 0) && (arrayFieldNumbers[row-1][col] != CELL_MINE)) {
-                arrayFieldNumbers[row-1][col]++;
+            if ((row > 0) && (arrayFieldNumbers[row-1][col].mine === false)) {
+                arrayFieldNumbers[row-1][col].number++;
             }
             //top diagonal right
-            if ((row > 0) && (col < width-1) && (arrayFieldNumbers[row-1][col+1] != CELL_MINE)) {
-                arrayFieldNumbers[row-1][col+1]++;
+            if ((row > 0) && (col < width-1) && (arrayFieldNumbers[row-1][col+1].mine === false)) {
+                arrayFieldNumbers[row-1][col+1].number++;
             }
             //right
-            if ((col < width-1) && (arrayFieldNumbers[row][col+1] != CELL_MINE)) {
-                arrayFieldNumbers[row][col + 1]++;
+            if ((col < width-1) && (arrayFieldNumbers[row][col+1].mine === false)) {
+                arrayFieldNumbers[row][col + 1].number++;
             }
             //bottom diagonal right
-            if ((row < height-1) && (col < width-1) && (arrayFieldNumbers[row+1][col+1] != CELL_MINE)) {
-                arrayFieldNumbers[row + 1][col + 1]++;
+            if ((row < height-1) && (col < width-1) && (arrayFieldNumbers[row+1][col+1].mine === false)) {
+                arrayFieldNumbers[row + 1][col + 1].number++;
             }
             //bottom center
-            if ((row < height-1) && (arrayFieldNumbers[row+1][col] != CELL_MINE)) {
-                arrayFieldNumbers[row + 1][col]++;
+            if ((row < height-1) && (arrayFieldNumbers[row+1][col].mine === false)) {
+                arrayFieldNumbers[row + 1][col].number++;
             }
             //bottom diagonal left
-            if ((row < height-1) && (col > 0) && (arrayFieldNumbers[row+1][col-1] != CELL_MINE)) {
-                arrayFieldNumbers[row + 1][col - 1]++;
+            if ((row < height-1) && (col > 0) && (arrayFieldNumbers[row+1][col-1].mine === false)) {
+                arrayFieldNumbers[row + 1][col - 1].number++;
             }
             //left
-            if ((col > 0) && (arrayFieldNumbers[row][col-1] != CELL_MINE)) {
-                arrayFieldNumbers[row][col - 1]++;
+            if ((col > 0) && (arrayFieldNumbers[row][col-1].mine === false)) {
+                arrayFieldNumbers[row][col - 1].number++;
             }
         }
     }
 }
-//document.write(arrayFieldNumbers.join());
 //console.log(arrayFieldNumbers);
 
 //right-clicked flags
-for (var row = 0; row < height; row++) {
-    for (var col = 0; col < width; col++) {
-        var eImgCell = eImages[row][col];
+for (row = 0; row < height; row++) {
+    for (col = 0; col < width; col++) {
+        eImgCell = eImages[row][col];
 
         eImgCell.addEventListener("contextmenu", function (event) {
         event.preventDefault();
@@ -129,8 +127,8 @@ for (var row = 0; row < height; row++) {
 }
 
 /*//right-clicked delete flag, paste field - no working
-for (var row = 0; row < height; row++) {
-    for (var col = 0; col < width; col++) {
+for (row = 0; row < height; row++) {
+    for (col = 0; col < width; col++) {
         var eImgCell2 = eImages[row][col];
 
         eImgCell.addEventListener("contextmenu", function (event) {
@@ -146,32 +144,33 @@ for (var row = 0; row < height; row++) {
 
 //Cell selection (click)
 var click = function (row, col) {
-    if (arrayFieldNumbers[row][col] === CELL_MINE) {
+    if (arrayFieldNumbers[row][col].mine) {
         eImages[row][col].src = "images/mine.jpg";
         setTimeout('alert("You lose! 0_o")', 100);
         setTimeout('document.location.reload()', 300);
     }   else {
-        eImages[row][col].src = "images/" + arrayFieldNumbers[row][col] + ".jpg";
+        eImages[row][col].src = "images/" + arrayFieldNumbers[row][col].number + ".jpg";
+        arrayFieldNumbers[row][col].opened = true;
         emptyCellsCount++;
 
         if (emptyCellsCount === (width * height - CELL_MINE)) {
             GameWin();
         }
 
-        if (arrayFieldNumbers[row][col] === 0) {
+        if (arrayFieldNumbers[row][col].number === 0) {
             findEmpty(row, col);
         }
     }
 }
 
-for (var row = 0; row < height; row++) {
-    for (var col = 0; col < width; col++) {
-        var eImgCell = eImages[row][col]; // eImgCell?
+for (row = 0; row < height; row++) {
+    for (col = 0; col < width; col++) {
+        var eImgCell = eImages[row][col]; // eImgCell? <img src="...">
 
         eImgCell.addEventListener("click",
             function (event) {
-                //event.preventDefault();
-                //event.stopPropagation();
+                event.preventDefault();
+                event.stopPropagation();
                 var element = event.target;
                 var col = parseInt(element.dataset.col);
                 var row = parseInt(element.dataset.row);
@@ -183,76 +182,80 @@ for (var row = 0; row < height; row++) {
 
 //searching and opening for empty cell
 var findEmpty = function (row, col) {
+
+    arrayFieldNumbers[row][col].opened = true;
+
     if (emptyCellsCount === (width * height - CELL_MINE)) {
         GameWin();
     }
+
     //check top diagonal left cell
-    if ((row > 0) && (col > 0)) {
-        eImages[row - 1][col - 1].src = "images/" + arrayFieldNumbers[row - 1][col - 1] + ".jpg";
+    if ((row > 0) && (col > 0) && (arrayFieldNumbers[row - 1][col - 1].opened != true)) {
+        eImages[row - 1][col - 1].src = "images/" + arrayFieldNumbers[row - 1][col - 1].number + ".jpg";
+        arrayFieldNumbers[row - 1][col - 1].opened = true;
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row - 1][col - 1] === 0) {
+        if (arrayFieldNumbers[row - 1][col - 1].number === 0) {
             findEmpty(row - 1, col - 1);
-        }*/
+        }
     }
     //check top cell
-    if (row > 0) {
-        eImages[row - 1][col].src = "images/" + arrayFieldNumbers[row - 1][col] + ".jpg";
+    if ((row > 0) && (arrayFieldNumbers[row - 1][col].opened != true)) {
+        eImages[row - 1][col].src = "images/" + arrayFieldNumbers[row - 1][col].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row - 1][col] = 0) {
+        if (arrayFieldNumbers[row - 1][col].number === 0) {
             findEmpty(row - 1, col);
-        }*/
+        }
     }
-
     //check top diagonal right cell
-    if ((row > 0) && (col < width - 1)) {
-        eImages[row - 1][col + 1].src = "images/" + arrayFieldNumbers[row - 1][col + 1] + ".jpg";
+    if ((row > 0) && (col < width - 1) && (arrayFieldNumbers[row - 1][col + 1].opened != true)) {
+        eImages[row - 1][col + 1].src = "images/" + arrayFieldNumbers[row - 1][col + 1].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row - 1][col + 1] = 0) {
+        if (arrayFieldNumbers[row - 1][col + 1].number === 0) {
             findEmpty(row - 1, col + 1);
-        }*/
+        }
     }
     //check right cell
-    if (col < width - 1) {
-        eImages[row][col + 1].src = "images/" + arrayFieldNumbers[row][col + 1] + ".jpg";
+    if ((col < width - 1) && (arrayFieldNumbers[row][col + 1].opened != true)) {
+        eImages[row][col + 1].src = "images/" + arrayFieldNumbers[row][col + 1].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row][col + 1] = 0) {
+        if (arrayFieldNumbers[row][col + 1].number === 0) {
             findEmpty(row, col + 1);
-        }*/
+        }
     }
     //check bottom diagonal right cell
-    if ((row < height - 1) && (col < width - 1)) {
-        eImages[row + 1][col + 1].src = "images/" + arrayFieldNumbers[row + 1][col + 1] + ".jpg";
+    if ((row < height - 1) && (col < width - 1) && (arrayFieldNumbers[row + 1][col + 1].opened != true)) {
+        eImages[row + 1][col + 1].src = "images/" + arrayFieldNumbers[row + 1][col + 1].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row + 1][col + 1] = 0) {
+        if (arrayFieldNumbers[row + 1][col + 1].number === 0) {
             findEmpty(row + 1, col + 1);
-        }*/
+        }
     }
-
     //check bottom cell
-    if (row < height - 1) {
-        eImages[row + 1][col].src = "images/" + arrayFieldNumbers[row + 1][col] + ".jpg";
+    if ((row < height - 1) && (arrayFieldNumbers[row + 1][col].opened != true)) {
+        eImages[row + 1][col].src = "images/" + arrayFieldNumbers[row + 1][col].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row + 1][col] = 0) {
+        if (arrayFieldNumbers[row + 1][col].number === 0) {
             findEmpty(row + 1, col);
-        }*/
+        }
     }
     //check bottom diagonal left cell
-    if ((row < height - 1) && (col > 0)) {
-        eImages[row + 1][col - 1].src = "images/" + arrayFieldNumbers[row + 1][col - 1] + ".jpg";
+    if ((row < height - 1) && (col > 0) && (arrayFieldNumbers[row + 1][col - 1].opened != true)) {
+        eImages[row + 1][col - 1].src = "images/" + arrayFieldNumbers[row + 1][col - 1].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row + 1][col - 1] = 0) {
+        if (arrayFieldNumbers[row + 1][col - 1].number === 0) {
             findEmpty(row + 1, col - 1);
-        }*/
+        }
     }
     //check left cell
-    if (col > 0) {
-        eImages[row][col - 1].src = "images/" + arrayFieldNumbers[row][col - 1] + ".jpg";
+    if ((col > 0) && (arrayFieldNumbers[row][col - 1].opened != true)) {
+        eImages[row][col - 1].src = "images/" + arrayFieldNumbers[row][col - 1].number + ".jpg";
         emptyCellsCount++;
-/*        if (arrayFieldNumbers[row][col - 1] = 0) {
+        if (arrayFieldNumbers[row][col - 1].number === 0) {
             findEmpty(row, col - 1);
-        }*/
+        }
     }
-}
+};
+
 
 
 
